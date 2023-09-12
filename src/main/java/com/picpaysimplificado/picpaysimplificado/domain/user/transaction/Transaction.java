bae1096a94e9901a47683c190a120186072129b6
Maria Eduarda Alves <malvesduarte444@gmail.com>
2023-09-12 20:40:26 -0300

    commit

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/transaction/Transaction.java
@@ -0,0 +1,34 @@
+package com.picpaysimplificado.picpaysimplificado.domain.user.transaction;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDataTime timestamp;
+    public Transaction() {
+
+    }
+}
